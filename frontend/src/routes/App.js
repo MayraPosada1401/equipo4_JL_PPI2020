a6c3e1c17fef8a6211ea3caa6b85d1971fa2efdc
Karolcuellar <karoleliana2003@gmail.com>
2020-10-14 20:14:15 -0500

    Se realizaron cambios en direcciones de componentes en la pagina de rutas

--- frontend/src/routes/App.js
+++ frontend/src/routes/App.js
@@ -15,6 +15,24 @@ import ElegirCursoCampe from '../pages/Elegircurso';
 import SeleccionarAlgunCurso from '../pages/seleccionaralguncurso';
 import MaterialCursoCampe from '../pages/MaterialCursoCampe';
 import MisPedidos from '../pages/MisPedidos';
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Casa from '../componentes/Casa';
+import Miscompras from '../componentes/Miscompras';
+import Mapa from '../componentes/Mapa';
+import App1 from '../componentes/App1';
+import Transportador from '../componentes/Transportador';
+import Quienes from '../componentes/Quienes';
+import Registro from '../componentes/Registro';
+import Regionestenden from '../componentes/Regionestenden';
+import Regiones2 from '../componentes/Regiones2';
+import Regiones4 from '../componentes/Regiones4';
+import Regiones3 from '../componentes/Regiones3';
+import Regiones5 from '../componentes/Regiones5';
+import Regiones6 from '../componentes/Regiones6';
+import Mapademasvendidos from '../componentes/Mapademasvendidos';
+import Misvaloraciones from '../componentes/Misvaloraciones';
+import Inciciarsesioncom from '../componentes/Inciciarsesioncom';
 
 export default function App() {
     return (
@@ -36,6 +54,23 @@ export default function App() {
           <Route exact path="/seleccionarcurso" component={SeleccionarAlgunCurso} />
           <Route exact path="/materialcurso" component={MaterialCursoCampe} />
           <Route exact path="/mispedidos" component={MisPedidos} />
+          <Route exact path="/" component={Casa}/>
+    <Route  path="/comprador" component={Miscompras}/>
+    <Route  path="/compradormapa" component={Mapa}/>
+    <Route path="/app1" component={App1}/>
+    <Route path="/transportador" component={Transportador}/>
+    <Route path="/registro" component={Registro}/>
+    <Route path="/quienes" component={Quienes}/>
+    <Route path="/regionestenden" component={Regionestenden}/>
+    <Route path="/regiones2" component={Regiones2}/>
+    <Route path="/regiones3" component={Regiones3}/>
+    <Route path="/regiones4" component={Regiones4}/>
+    <Route path="/regiones5" component={Regiones5}/>
+    <Route path="/regiones6" component={Regiones6}/>
+    <Route path="/mapas2" component={Mapademasvendidos}/>
+    <Route path="/conocenos" component={Misvaloraciones}/>
+    <Route path="/iniciosesion" component={Inciciarsesioncom}/>
+
       </Switch>    
 
       </BrowserRouter>
