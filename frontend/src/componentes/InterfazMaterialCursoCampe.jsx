329e77d668d189627134705caa817f9413f3e09f
MayraPosada1401 <mayravanessaposadaalvarez@gmail.com>
2020-11-24 18:49:07 -0500

    Se hacen más ártes del crud (Tipo método get)

--- frontend/src/componentes/InterfazMaterialCursoCampe.jsx
+++ frontend/src/componentes/InterfazMaterialCursoCampe.jsx
@@ -13,7 +13,7 @@ export default class InterfazMaterialCursoCampe extends Component {
 }
 
 peticionGet2=()=>{
-    axios.get('https://semana9jl.karolcuellar.repl.co/api/curso/:id').then(res =>{
+    axios.get('https://semana9jl.karolcuellar.repl.co/api/cursos').then(res =>{
         console.log(res.data);
         this.setState({
           dataInfoCurso: res.data
@@ -33,15 +33,17 @@ return (
     
     <div className="MaterialCursoCampe2">
             <div className="blog-main">
-            <h1 className="border-bottom titulodelcurso-materialcursocampe">
-            pet2.nombre_curso
-            </h1>
+              {datosInfo.map((pet2) =>{
+                return(
+                  <h1 className="border-bottom titulodelcurso-materialcursocampe">{pet2.nombre_curso}</h1>
+                )
+              })}
+            
   
             <div className="blog-post-1-materialcursocampe">
-               <div>
-               <h2 className="blog-post-title1-materialcursocampe font-italic">¿Cuánto puedo invertir por un pesticida?</h2>
-           <p className="blog-post-meta">pet2.fecha_curso</p>
-  
+              <div>
+              <h2 className="blog-post-title1-materialcursocampe font-italic">¿Cuánto puedo invertir por un pesticida?</h2>
+
               <p className="parrafosdematerialcursocampe">Al momento de comprar un pesticida muchas veces nos preguntamos si su precio influye en su calidad y si en realidad es óptimo para el cuidado de nuestros cultivos, por lo cual nos inclinamos por unos precios y en realidad al momento de probarlo nos damos cuenta de que no son tan buenos y benéficos para el desarrollo de nuestros cultivos y esto nos causa un dolor de cabeza.</p>
               <hr/>
               <p className="parrafosdematerialcursocampe">Hay muchos aspectos que se deben de tener en cuenta para comprar pesticidas que nos permitan fortalecer las cosechas y su producción. El precio no siempre es algo que defina la calidad y la forma en que el pesticida va a influir para un desarrollo de productos eficiente. Muchas veces los pesticidas económicos sirven más que los costosos, pero es por los componentes que contienen los cuales influyen de manera diversa en los cultivos.</p>
@@ -54,38 +56,37 @@ return (
                 <li className="parrafosdematerialcursocampe">Almacenamiento incorrecto de pesticidas.</li>
                 <li className="parrafosdematerialcursocampe">No utilizar equipos de protección, o utilización errónea.</li>
               </ul>
-               </div> 
-              
-           
-          <aside className="blog-sidebar-materialcursocampe">
-            <div className="bg-light rounded">
-              <h4 className="font-italic padding-datocurioso-materialcursocampe">Dato Curioso</h4>
-              <p className="parrafosdematerialcursocampe padding-datocurioso-materialcursocampe">Las fresas son las frutas con más pesticidas, en cantidad y en variedad -36 pesticidas diferentes son utilizados en estas deliciosas frutas-. Esto hace que casi el 90% de la producción muestre signos de químicos por encima del nivel saludable. Otros frutos rojos y bayas tienen similares cantidades, aunque los niveles de las fresas son realmente alarmantes.</p>
-            </div>
+              </div> 
+               
+              <aside className="blog-sidebar-materialcursocampe">
+                <div className="bg-light rounded">
+                 <h4 className="font-italic padding-datocurioso-materialcursocampe">Dato Curioso</h4>
+                 <p className="parrafosdematerialcursocampe padding-datocurioso-materialcursocampe">Las fresas son las frutas con más pesticidas, en cantidad y en variedad -36 pesticidas diferentes son utilizados en estas deliciosas frutas-. Esto hace que casi el 90% de la producción muestre signos de químicos por encima del nivel saludable. Otros frutos rojos y bayas tienen similares cantidades, aunque los niveles de las fresas son realmente alarmantes.</p>
+                </div>
   
-            <div>
-              <h4 className="font-italic padding-datocurioso-materialcursocampe">Documentación</h4>
-              <ol className="list-unstyled mb-0">
-                <li className="padding-listas-materialcursocampe"><a href="#">Mejoresmarcasdepesticidas.pdf</a></li>
-                <li className="padding-listas-materialcursocampe"><a href="#">Tiposdepesticidas.docx</a></li>
-                <li className="padding-listas-materialcursocampe"><a href="#">Pesticidaseconómicos.docx</a></li>
-                <li className="padding-listas-materialcursocampe"><a href="#">NoticiaPesticida.png</a></li>
-                <li className="padding-listas-materialcursocampe"><a href="#">ExplicaciónInfluenciapesticidas.mp4</a></li>
-                <li className="padding-listas-materialcursocampe"><a href="#">AudioExpositivopesticidas.mp3</a></li>
-                <li className="padding-listas-materialcursocampe"><a href="#">Influenciaeconómicadeunpesticida.pdf</a></li>
-              </ol>
-            </div>
+                <div>
+                 <h4 className="font-italic padding-datocurioso-materialcursocampe">Documentación</h4>
+                 <ol className="list-unstyled mb-0">
+                  <li className="padding-listas-materialcursocampe"><a href="#">Mejoresmarcasdepesticidas.pdf</a></li>
+                  <li className="padding-listas-materialcursocampe"><a href="#">Tiposdepesticidas.docx</a></li>
+                  <li className="padding-listas-materialcursocampe"><a href="#">Pesticidaseconómicos.docx</a></li>
+                  <li className="padding-listas-materialcursocampe"><a href="#">NoticiaPesticida.png</a></li>
+                  <li className="padding-listas-materialcursocampe"><a href="#">ExplicaciónInfluenciapesticidas.mp4</a></li>
+                  <li className="padding-listas-materialcursocampe"><a href="#">AudioExpositivopesticidas.mp3</a></li>
+                  <li className="padding-listas-materialcursocampe"><a href="#">Influenciaeconómicadeunpesticida.pdf</a></li>
+                 </ol>
+                </div>
   
-            <div className="margin-links-documentoscursocampe">
-              <h4 className="font-italic padding-datocurioso-materialcursocampe">Links Externos</h4>
-              <ol className="list-unstyled">
-                <li className="padding-listas-materialcursocampe"><a href="#">Video de Youtube de uso de Pesticidas</a></li>
-                <li className="padding-listas-materialcursocampe"><a href="#">Noticia: Desarrollo de Pesticidas orgánicos y económicos</a></li>
-                <li className="padding-listas-materialcursocampe"><a href="#">Texto de uso de pesticidas económicos para plagas</a></li>
-              </ol>
-            </div>
-          </aside> 
-         </div>
+                <div className="margin-links-documentoscursocampe">
+                  <h4 className="font-italic padding-datocurioso-materialcursocampe">Links Externos</h4>
+                  <ol className="list-unstyled">
+                    <li className="padding-listas-materialcursocampe"><a href="#">Video de Youtube de uso de Pesticidas</a></li>
+                    <li className="padding-listas-materialcursocampe"><a href="#">Noticia: Desarrollo de Pesticidas orgánicos y económicos</a></li>
+                    <li className="padding-listas-materialcursocampe"><a href="#">Texto de uso de pesticidas económicos para plagas</a></li>
+                  </ol>
+                </div>
+             </aside> 
+           </div>
         </div>
 
     </div>  
