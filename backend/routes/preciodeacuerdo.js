268eb80cd4d8818dce324ba8f7fbfb17f30bd752
Mayra Vanessa Posada Alvarez <66489720+MayraPosada1401@users.noreply.github.com>
2020-11-27 00:44:35 -0500

    Update preciodeacuerdo.js

--- backend/routes/preciodeacuerdo.js
+++ backend/routes/preciodeacuerdo.js
@@ -6,7 +6,7 @@ const mysqlConnection = require ('../db/db');
 
 router.get('/preciosdeacuerdos', (req, res) => {
      
-  mysqlConnection.query('SELECT * FROM precio_acuerdo ', (err, rows, fields) => {
+  mysqlConnection.query('SELECT * FROM precio_acuerdo2 ', (err, rows, fields) => {
       if (!err) {
         res.json(rows);
       } else {
@@ -19,12 +19,12 @@ router.get('/preciosdeacuerdos', (req, res) => {
 
 router.post('/nuevopreciodeacuerdo',(req,res)=>{
 
-const {precio_nuevo} = req.body;
+const {precio_nuevo, id_registros} = req.body;
 
-let precio_acuerdo = [precio_nuevo];
+let precio_acuerdo = [precio_nuevo, id_registros];
 
-let nuevoacuerdo = `INSERT INTO precio_acuerdo (precio_nuevo)
-                  VALUES(?)`;
+let nuevoacuerdo = `INSERT INTO precio_acuerdo2 (precio_nuevo, id_registros)
+                  VALUES(?,?)`;
 mysqlConnection.query(nuevoacuerdo, precio_acuerdo, (err, results, fields) => {
   if (err) {
     return console.error(err.message);
