adb657512275e22336b831e8225f6dc3751a6450
Mayra Vanessa Posada Alvarez <66489720+MayraPosada1401@users.noreply.github.com>
2020-11-27 00:42:38 -0500

    Update chat.js

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -6,7 +6,7 @@ const mysqlConnection = require ('../db/db');
 // METODO GET - Para el despliegue de las conversaciones y dialogos anteriores
 router.get('/conversaciones', (req, res) => {
      
-  mysqlConnection.query('SELECT * FROM chat ', (err, rows, fields) => {
+  mysqlConnection.query('SELECT * FROM chat_usuario ', (err, rows, fields) => {
       if (!err) {
         res.json(rows);
       } else {
@@ -18,12 +18,12 @@ router.get('/conversaciones', (req, res) => {
 // METODO POST - Para iniciar un nuevo dialogo, mensaje
 router.post('/nuevaconversacion',(req,res)=>{
 
-const {conversacion} = req.body;
+const {conversacion, id_registro} = req.body;
 
-let conversacionusuario = [conversacion];
+let conversacionusuario = [conversacion, id_registro];
 
-let nuevoRegistro = `INSERT INTO chat (conversacion)
-                  VALUES(?)`;
+let nuevoRegistro = `INSERT INTO chat_usuario (conversacion, id_registro)
+                  VALUES(?,?)`;
 mysqlConnection.query(nuevoRegistro, conversacionusuario, (err, results, fields) => {
   if (err) {
     return console.error(err.message);
